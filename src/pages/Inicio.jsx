8464675a75af3d11df40e6fdc53bf1e36df9c9f5
Pauloglhr <pauloglhr@outlook.com>
2025-06-16 22:40:52 -0300

    chore: implementação da lib react-router-dom

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,9 @@
+
+
+const Inicio = () => {
+    return (
+        <h1>Olá, bem vindo ao meu portifólio!</h1>
+    )
+}
+
+export default Inicio
---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Inicio from './pages/Inicio'
+
+function AppRoutes() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Inicio />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default AppRoutes
