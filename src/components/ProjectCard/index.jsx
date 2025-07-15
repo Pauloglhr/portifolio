fc6c9600a22f72d50015155a6b4d0484db3f0ff5
Pauloglhr <pauloglhr@outlook.com>
2025-07-15 18:48:50 -0300

    feat: adiciona seção flexbox que contem os cards

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,33 +1,41 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const StyledCard = styled.a`
-  background-image: ${(props) => `url(${props.$imagem})`};
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center center;
-  height: ${(props) => (props.$wide ? "40vh" : "60vh")};
-  width: ${(props) => (props.$wide ? "30vw" : "20vw")};
-  min-width: 30vw;
+const StyledCard = styled.div`
+  /* border: 1px solid #333; */
+  border-radius: 8px;
+  overflow: hidden;
   cursor: pointer;
+  transition: transform 0.5s ease;
+  width: 300px;
+
   &:hover {
-    span {
+    transform: scale(1.03);
+    h3 {
       display: block;
       position: fixed;
-      height: 100%;
       top: 50%;
       left: 50%;
     }
   }
 
-  span {
+  img {
+    max-height: 500px;
+    width: 300px;
+  }
+
+  h3 {
     display: none;
   }
 `;
 
-const ProjectCard = ({ imagem, wide = false, titulo }) => {
+const ProjectCard = ({ imagem, titulo }) => {
   return (
     <StyledCard>
-    
+      <Link to="/About">
+        <img src={imagem} />
+        <h3>{titulo}</h3>
+      </Link>
     </StyledCard>
   );
 };
--- src/components/ProjectSection/index.jsx
+++ src/components/ProjectSection/index.jsx
@@ -1,20 +1,72 @@
+import { useTransitionNavigate } from "@/contexts/NavigationContext";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import imagem1 from "@/assets/imagem.png";
 
 const StyledProjectSection = styled.section`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  gap: 2rem;
-  padding: 5rem 2rem;
+  min-height: 100vh;
+  padding: 25px 80px;
+  h3 {
+    text-align: center;
+    font-size: 4rem;
+  }
+
+  .card {
+    padding: 1.5rem;
+    border-radius: 8px;
+    color: white;
+    transition: ease 0.2s;
+
+    &:hover {
+      transform: scale(1.02);
+    }
+  }
+
+  .container-flex {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 5rem;
+    padding: 1rem;
+    align-items: center;
+  }
+  .container-flex .card {
+    height: 400px;
+    width: 300px;
+    background-color: #4a90e2;
+    background-image: url(${imagem1});
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+  .container-flex .card-wide {
+    height: 250px;
+    width: 500px;
+    background-color: #7ed321;
+    background-image: url(${imagem1});
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 `;
 
 const ProjectSection = () => {
+  const { navigateWithTransition } = useTransitionNavigate();
+
+  const handleLinkClick = (e, to) => {
+    e.preventDefault();
+    navigateWithTransition(to);
+  };
   return (
-    <>
-    <h3>PROJETOS</h3>
     <StyledProjectSection>
-      
+      <h3>PROJETOS</h3>
+      <div className="container-flex">
+        <Link to="/About" onClick={(e) => handleLinkClick(e, "/About")}>
+          <div className="card"><span>Projeto1</span></div>
+        </Link>
+        <Link to="/About" onClick={(e) => handleLinkClick(e, "/About")}>
+          <div className="card card-wide"><span>Projeto2</span></div>
+        </Link>
+      </div>
     </StyledProjectSection>
-    </>
   );
 };
 
