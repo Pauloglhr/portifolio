6e3859e4865c3baddffbcf64a34dffc0eda0c342
Pauloglhr <pauloglhr@outlook.com>
2025-07-20 23:31:47 -0300

    feat: adiciona layout de card de projetos

--- src/components/ProjectSection/index.jsx
+++ src/components/ProjectSection/index.jsx
@@ -1,6 +1,7 @@
 import { useTransitionNavigate } from "@/contexts/NavigationContext";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import bgProjeto1 from "@/assets/projetoImage/1/imagem.png";
 
 const StyledProjectSection = styled.section`
   min-height: 100vh;
@@ -17,8 +18,31 @@ const StyledProjectSection = styled.section`
     gap: 5rem;
     padding: 1rem;
     align-items: center;
+
+    a {
+      text-decoration: none;
+    }
+
   }
 `;
+
+const StyledCard = styled.div`
+figcaption {
+  display: none;
+ }
+
+figure {
+  &:hover {
+    figcaption {
+      display: block;
+      position: fixed;
+      left: 50%;
+      top: 50%;
+    }
+  }
+} 
+
+`
 
 const ProjectSection = () => {
   const { navigateWithTransition } = useTransitionNavigate();
@@ -32,18 +56,17 @@ const ProjectSection = () => {
     <StyledProjectSection>
       <h3>PROJETOS</h3>
       <div className="container-flex">
-        <ul>
-          <li>
-            <Link
-              to="/projetos/pesquisa-engajamento"
-              onClick={(e) =>
-                handleLinkClick(e, "/projetos/pesquisa-engajamento")
-              }
-            >
-              Projeto 1
-            </Link>
-          </li>
-        </ul>
+        <Link
+          to="/projetos/pesquisa-engajamento"
+          onClick={(e) => handleLinkClick(e, "/projetos/pesquisa-engajamento")}
+        >
+          <StyledCard>
+            <figure>
+              <img src={bgProjeto1} alt="" />
+              <figcaption>Pesquisa de engajamento.</figcaption>
+            </figure>
+          </StyledCard>
+        </Link>
       </div>
     </StyledProjectSection>
   );
